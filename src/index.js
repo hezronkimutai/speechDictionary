ed82be6b26eb4a650ab1d81afc2aa9a7c33e91ec
hezronkimutai <hezron.kimutai@andela.com>
2019-12-17 09:41:58 +0200

    ch(set up redux for a basic app): setup redux for a basic todo app [ #170331469 Finishes]

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import App from "./App";
 
 const rootElement = document.getElementById("react-app");
 
 render(
-  
-    <App/>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   rootElement
 );
